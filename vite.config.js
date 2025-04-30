e327bf5d7561dae75f60728087671531b55c7ef8
IrvingJT1 <irvinjt@gmail.com>
2025-04-29 20:39:53 -0600

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { webcrypto } from 'crypto'; // ✅ esta es la clave
 
 // https://vite.dev/config/
 export default defineConfig({
   define:{
-    'globalThis.crypto': 'require("crypto").webcrypto'
+    globalThis: {
+      crypto: webcrypto // ✅ aquí se inyecta correctamente
+    }
   },
   base: "/gestion_citas_hook/",
   plugins: [
